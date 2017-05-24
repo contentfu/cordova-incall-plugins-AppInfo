7ac173777b8f0415ec05298e81054d796c521345
Content Fu <878286256@qq.com>
2017-05-24 11:24:13 +0800

    c
    c

--- src/ios/AppInfo.m
+++ src/ios/AppInfo.m
@@ -1,5 +1,146 @@
 #import "AppInfo.h"
 @implementation AppInfo
 
+//获取项目信息
+- (void)getAppInfo:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)getVersion:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)getIdentifier:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)share:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void) shareCommon:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)createShare:(CDVInvokedUrlCommand*)command{
+    
+}
+
+//设备信息
+- (void)getDeviceInfo:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void) openCarLocationView:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void) showCarLocationViewLoading:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void) stopCarLocationViewLoading:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void) getCarError:(CDVInvokedUrlCommand*)command{
+    
+}
+
+//- (void) startCarLocationViewPassword:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void) showStatus:(CDVInvokedUrlCommand*)command{
+    
+}
+- (void)showIndicator:(CDVInvokedUrlCommand*)command{
+    
+}
+- (void)dismissIndicator:(CDVInvokedUrlCommand*)command{
+    
+}
+/**
+ *  打开周边服务页面
+ */
+- (void)openSurService:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)getFenceDatas:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)fenceList:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)openTeamTravel:(CDVInvokedUrlCommand *)command{
+    
+}
+
+- (void)showConfirmDilaog:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)getAllTeamMember:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)LoginIM:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)quitTeam:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)createTeamSuccess:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)removeTeam:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)updateTeam:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)joinTeam:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)popToRootView:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)updateImageCode:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)fileList:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)openPdf:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)changePDFName:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)isWifi:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)getUniqueId:(CDVInvokedUrlCommand*)command{
+    
+}
+
+- (void)fenceIdSuccess:(CDVInvokedUrlCommand*)command{
+    
+}
 
 @end
